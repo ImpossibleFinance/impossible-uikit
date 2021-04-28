d8519393dd532b58119ab2fe10134cb5f87082d7
Accelerated Monkey <acceleratedape@protonmail.com>
2021-04-28 14:08:26 +0800

    make navigation bar responsive

---
+++ src/widgets/Menu/components/MobileUserBlock.tsx
@@ -0,0 +1,113 @@
+import React, { useCallback, useState } from "react";
+import styled from "styled-components";
+import { Box, Flex } from "../../../components/Box";
+import Button from "../../../components/Button/Button";
+import Wallet from "../../../components/Svg/Icons/Wallet";
+import { useWalletModal } from "../../WalletModal";
+import { Login } from "../../WalletModal/types";
+
+interface Props {
+  account?: string;
+  useStaxBalance?: () => string;
+  useBnbBalance?: () => string;
+  login: Login;
+  logout: () => void;
+}
+
+const UserBlockWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background: transparent;
+  padding: 20px;
+`;
+
+const WalletButton = styled(Button)`
+  border-radius: 30px;
+  background: ${({ theme }) => theme.colors.invertedContrast};
+  color: ${({ theme }) => theme.colors.text};
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.09);
+`;
+
+const AccountWrapper = styled.div`
+  background: ${({ theme }) => theme.colors.backgroundDisabled};
+  border-radius: 30px;
+  padding: 3px;
+  display: flex;
+  flex-direction: row;
+`;
+
+const ShowBalance = styled.div`
+  color: ${({ theme }) => theme.colors.textSubtle};
+`;
+
+const Label = styled.div`
+  color: ${({ theme }) => theme.colors.text};
+  font-weight: 700;
+`;
+const WalletIcon = () => (
+  <Box ml="8px">
+    <Wallet />
+  </Box>
+);
+
+const MobileUserBlock: React.FC<Props> = ({ account, useStaxBalance, useBnbBalance, login, logout }) => {
+  const { onPresentConnectModal, onPresentAccountModal } = useWalletModal(login, logout, account);
+  const accountEllipsis = account ? `${account.substring(0, 4)}...${account.substring(account.length - 4)}` : null;
+  const staxBalance = useStaxBalance && useStaxBalance();
+  const bnbBalance = useBnbBalance && useBnbBalance();
+  const [showBalance, setShowBalance] = useState(false);
+  const showBalanceClick = useCallback(() => {
+    setShowBalance(!showBalance);
+  }, [setShowBalance, showBalance]);
+  return (
+    <UserBlockWrapper>
+      {account ? (
+        <Flex flexDirection="column" flex={1}>
+          <Flex flexDirection="row" justifyContent="space-between" alignItems="center">
+            <AccountWrapper>
+              <Button
+                scale="sm"
+                variant="tertiary"
+                onClick={() => {
+                  onPresentAccountModal();
+                }}
+              >
+                {accountEllipsis}
+              </Button>
+            </AccountWrapper>
+            <ShowBalance onClick={showBalanceClick}>{showBalance ? "Hide Balance" : "Show balance"}</ShowBalance>
+          </Flex>
+          {showBalance && (
+            <Flex mt="12px" flexDirection="row" justifyContent="space-between" alignItems="center">
+              <Label>STAX</Label>
+              <Label>{staxBalance}</Label>
+            </Flex>
+          )}
+          {showBalance && (
+            <Flex mt="12px" flexDirection="row" justifyContent="space-between" alignItems="center">
+              <Label>BNB</Label>
+              <Label>{bnbBalance}</Label>
+            </Flex>
+          )}
+        </Flex>
+      ) : (
+        <Flex justifyContent="center">
+          <WalletButton
+            height="42px"
+            width="230px"
+            scale="md"
+            endIcon={<WalletIcon />}
+            onClick={() => {
+              onPresentConnectModal();
+            }}
+          >
+            Connect Wallet
+          </WalletButton>
+        </Flex>
+      )}
+    </UserBlockWrapper>
+  );
+};
+
+export default MobileUserBlock;
--- src/widgets/Menu/components/UserBlock.tsx
+++ src/widgets/Menu/components/UserBlock.tsx
@@ -1,23 +1,23 @@
-import BigNumber from 'bignumber.js'
-import React from 'react'
-import styled from 'styled-components'
-import { getBalanceNumber } from 'utils/formatBalance'
-import Button from '../../../components/Button/Button'
-import { useWalletModal } from '../../WalletModal'
-import { Login } from '../../WalletModal/types'
+import React from "react";
+import styled from "styled-components";
+import Button from "../../../components/Button/Button";
+import { useWalletModal } from "../../WalletModal";
+import { Box } from "../../../components/Box";
+import { Login } from "../../WalletModal/types";
+import Wallet from "../../../components/Svg/Icons/Wallet";
 
 interface Props {
-  account?: string
-  useStaxBalance?: () => BigNumber
-  useBnbBalance?: () => BigNumber
-  login: Login
-  logout: () => void
+  account?: string;
+  useStaxBalance?: () => string;
+  useBnbBalance?: () => string;
+  login: Login;
+  logout: () => void;
 }
 
 const UserBlockWrapper = styled.div`
   display: flex;
   flex-direction: row;
-`
+`;
 
 const StaxBalance = styled.div`
   background: linear-gradient(249.75deg, #050a5a -14.92%, #0617bf 90.68%);
@@ -25,7 +25,7 @@ const StaxBalance = styled.div`
   border-radius: 19px;
   color: ${({ theme }) => theme.colors.invertedContrast};
   margin-right: 12px;
-`
+`;
 
 const AccountWrapper = styled.div`
   background: ${({ theme }) => theme.colors.backgroundDisabled};
@@ -33,39 +33,52 @@ const AccountWrapper = styled.div`
   padding: 3px;
   display: flex;
   flex-direction: row;
-`
+`;
 
 const BnbBalance = styled.div`
   display: flex;
   align-items: center;
   padding: 0 9px 0 13px;
-`
+`;
+
+const WalletButton = styled(Button)`
+  border-radius: 30px;
+  background: ${({ theme }) => theme.colors.invertedContrast};
+  color: ${({ theme }) => theme.colors.text};
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.09);
+`;
+
+const WalletIcon = () => (
+  <Box ml="8px">
+    <Wallet />
+  </Box>
+);
 
 const UserBlock: React.FC<Props> = ({ account, useBnbBalance, useStaxBalance, login, logout }) => {
-  const { onPresentConnectModal, onPresentAccountModal } = useWalletModal(login, logout, account)
-  const accountEllipsis = account ? `${account.substring(0, 4)}...${account.substring(account.length - 4)}` : null
-  const staxBalance = useStaxBalance()
-  const bnbBalance = useBnbBalance()
+  const { onPresentConnectModal, onPresentAccountModal } = useWalletModal(login, logout, account);
+  const accountEllipsis = account ? `${account.substring(0, 4)}...${account.substring(account.length - 4)}` : null;
+  const staxBalance = useStaxBalance && useStaxBalance();
+  const bnbBalance = useBnbBalance && useBnbBalance();
   return (
     <div>
       {account ? (
         <UserBlockWrapper>
           {staxBalance ? (
             <StaxBalance>
-              <b>{getBalanceNumber(staxBalance).toPrecision(4)}</b> STAX
+              <b>{staxBalance}</b> STAX
             </StaxBalance>
           ) : null}
           <AccountWrapper>
             {bnbBalance ? (
               <BnbBalance>
-                <b>{getBalanceNumber(bnbBalance).toPrecision(4)}</b> BNB
+                <b>{bnbBalance}</b> BNB
               </BnbBalance>
             ) : null}
             <Button
               scale="sm"
               variant="tertiary"
               onClick={() => {
-                onPresentAccountModal()
+                onPresentAccountModal();
               }}
             >
               {accountEllipsis}
@@ -73,17 +86,20 @@ const UserBlock: React.FC<Props> = ({ account, useBnbBalance, useStaxBalance, lo
           </AccountWrapper>
         </UserBlockWrapper>
       ) : (
-        <Button
-          scale="sm"
+        <WalletButton
+          height="42px"
+          width="200px"
+          scale="md"
+          endIcon={<WalletIcon />}
           onClick={() => {
-            onPresentConnectModal()
+            onPresentConnectModal();
           }}
         >
-          Connect
-        </Button>
+          Connect Wallet
+        </WalletButton>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default React.memo(UserBlock, (prevProps, nextProps) => prevProps.account === nextProps.account)
+export default React.memo(UserBlock, (prevProps, nextProps) => prevProps.account === nextProps.account);
