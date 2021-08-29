3921bed1ab1a1150a9312f671fa8268326967728
polarized.dino@gmail.com <polarized.dino@gmail.com>
2021-08-29 23:14:41 +0800

    Remove border bottom

--- src/widgets/Menu/Menu.tsx
+++ src/widgets/Menu/Menu.tsx
@@ -31,7 +31,6 @@ const StyledNav = styled.nav<{ showMenu: boolean }>`
   width: 100%;
   height: ${MENU_HEIGHT}px;
   background-color: ${({ theme }) => theme.nav.background};
-  border-bottom: solid 2px rgba(133, 133, 133, 0.1);
   z-index: 20;
   transform: translate3d(0, 0, 0);
 `;
