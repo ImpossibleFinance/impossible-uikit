d50f83cb342b7befa718ef686aee49cbbf84b51e
polarized.dino@gmail.com <polarized.dino@gmail.com>
2021-11-06 15:43:55 +0800

    Add unsupported modal

---
+++ src/widgets/WalletModal/UnsupportedModal.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Text from '../../components/Text/Text'
+import Flex from '../../components/Box/Flex'
+import { Modal } from '../Modal'
+import { Network } from './types'
+import Warning from './icons/Warning'
+
+interface Props {
+  onDismiss?: () => void
+  networks?: Network[]
+}
+
+const NetworkBox = styled(Flex)`
+  border: 1px solid #E9EBEC;
+  box-sizing: border-box;
+  border-radius: 12px;
+  padding: 8px 24px;
+`
+
+const UnsupportedModal: React.FC<Props> = ({ onDismiss = () => null, networks = [] }) => {
+  return (
+    <Modal title="Network Unavailable" onDismiss={onDismiss} style={{ borderRadius: '16px' }} bodyPadding="0px 24px 24px 24px">
+      <Flex justifyContent="center">
+        <Warning width="84px" />
+      </Flex>
+      <Flex marginTop="36px" marginBottom="24px" justifyContent="center">
+        Please connect to one of our supported networks.
+      </Flex>
+      {
+        networks.length > 0 && <>
+          {networks.map((network, index) => {
+            return (<NetworkBox style={{ cursor: "pointer" }} alignItems="center" marginTop={index > 0 ? "12px" : "0px"} onClick={() => {
+              if (network?.switchNetworkCallback) {
+                network.switchNetworkCallback();
+              }
+              onDismiss();
+            }}>
+              <img width="40px" alt={network.chainID.toString()} src={network.iconSrc} />
+              <Text
+                fontSize="16px"
+                marginLeft="10px"
+                fontWeight={500}
+              >
+                {network.fullName || network.name}
+              </Text>
+            </NetworkBox>)
+          })}
+        </>
+      }
+    </Modal >
+  )
+}
+
+export default UnsupportedModal
