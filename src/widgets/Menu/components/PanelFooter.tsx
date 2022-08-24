0057c9cf0f4a3933beb433d33362b30c3ae4fc89
Bogdan Bogdanov <bogdanov_bogdanv@mail.ru>
2022-08-24 07:27:01 +0300

    Fix linter errors: remove unused imports

--- src/widgets/Menu/components/PanelFooter.tsx
+++ src/widgets/Menu/components/PanelFooter.tsx
@@ -5,7 +5,6 @@ import IconButton from '../../../components/Button/IconButton'
 import { MENU_ENTRY_HEIGHT } from '../config'
 import { PanelProps, PushedProps } from '../types'
 import CakePrice from './CakePrice'
-import ThemeSwitcher from './ThemeSwitcher'
 import SocialLinks from './SocialLinks'
 import LangSelector from './LangSelector'
 
